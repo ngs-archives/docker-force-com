cb6a5f9d75ed7d95fc5b138a633bab889c4b2c8e
Atsushi Nagase <a@ngs.io>
2017-04-10 12:58:59 +0900

    Display compile error

--- entrypoint/TestRunner.java
+++ entrypoint/TestRunner.java
@@ -6,12 +6,17 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class TestRunner {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
 
     public static void main(String[] args) throws ConnectionException, IOException {
 
@@ -68,7 +73,62 @@ public class TestRunner {
         request.setRunTestsRequest(runTestsRequest);
 
         CompileAndTestResult result = connector.compileAndTest(request);
-        System.out.println(result);
-        assert (result.isSuccess());
+
+        Integer maxNameLength = 0;
+        Integer maxProblemLength = 0;
+        List<Object[]> rows = new ArrayList<Object[]>();
+
+        for (CompileClassResult cls : result.getClasses()) {
+            if (cls.isSuccess()) {
+                continue;
+            }
+            String name = cls.getName();
+            String problem = cls.getProblem();
+            Integer line = cls.getLine();
+            Integer len = name == null ? 0 : name.length();
+            if (maxNameLength < len) {
+                maxNameLength = len;
+            }
+            len = problem == null ? 0 : problem.length();
+            if (maxProblemLength < len) {
+                maxProblemLength = len;
+            }
+            rows.add(new Object[] { name, line, problem });
+        }
+        if (rows.size() > 0) {
+            System.out.println(ANSI_RED + "[ Compile Error ]"  + ANSI_RESET);
+            printTable(rows,
+                new Integer[] { maxNameLength, 5, maxProblemLength },
+                new String[] { "Name", "Line", "Problem" });
+        }
+        if (!result.isSuccess()) {
+            System.exit(-1);
+        }
+    }
+
+
+    private static void printTable(List<Object[]> rows, Integer[] columnWidth, String [] columnNames) {
+        List<String> topBarList = new ArrayList<String>();
+        List<String> sepBarList = new ArrayList<String>();
+        List<String> btmBarList = new ArrayList<String>();
+        List<String> rowFmtList = new ArrayList<String>();
+        for (Integer i = 0; i < columnWidth.length; i++) {
+            Integer w = columnWidth[i];
+            String bar = String.join("", Collections.nCopies(w, "─"));
+            topBarList.add(bar);
+            sepBarList.add(bar);
+            btmBarList.add(bar);
+            rowFmtList.add("%" + Integer.toString(i + 1) + "$-" + w + "s");
+        }
+        String rowFmt = "│ " + String.join(" │ ", rowFmtList) + " │";
+        List<String> out = new ArrayList<String>();
+        out.add("┌─" + String.join("─┬─", topBarList) + "─┐");
+        out.add(String.format(rowFmt, (Object[]) columnNames));
+        out.add("├─" + String.join("─┼─", sepBarList) + "─┤");
+        for (Object[] row : rows) {
+          out.add(String.format(rowFmt, row));
+        }
+        out.add("└─" + String.join("─┴─", btmBarList) + "─┘");
+        System.out.println(String.join("\n", out));
     }
 }
